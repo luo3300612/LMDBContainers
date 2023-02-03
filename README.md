8f8ee6ae22c7d6878d882e873ebc63059ce03fcc
骆云鹏 <591486669@qq.com>
2023-02-03 19:32:06 +0800

    single process and multiprocess lmdblist

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # LMDBContainers
+
 ## 问题
+
 超大规模数据的多卡多num_worker情况下，对于数据annotation信息的拷贝会造成极大的内存占用，本repo希望通过LMDB将大的容器对象cache到硬盘上，以避免上述问题
 
 ![OOM示例](https://raw.github.com/luo3300612/LMDBContainers/master/assets/oom_fig.png)
 
 ## 注意
+
 * LMDB写入时，必须保证没有任何txn在执行读取操作
 
 ## T0
-* 解决多进程下，LMDB list iter的问题：主要是由于多进程之间，LMDBList ready时间不一样，进程A ready后开始读数据了，但进程B刚打开LMDB，并修改了其中的进程计数器
 
+* 解决多进程下，LMDB list iter的问题：主要是由于多进程之间，LMDBList ready时间不一样，进程A ready后开始读数据了，但进程B刚打开LMDB，并修改了其中的进程计数器，Done
+* 依赖pytorch内置的同步机制，写一个初版的lmdblist，其进程间同步由pytorch来保证
 
 ## LMDB List
 
@@ -17,6 +21,7 @@
 
 * 单进程下，直接wrap list得到LMDB list,后续功能与list完全一致
 * 多进程下，保证写入安全，多进程同步，提供给dataloader使用
+* 仅让主进程写入，其他进程直接跳过，每次数据更新后删除之前的lmdb
 
 ### 测试
 
@@ -31,3 +36,10 @@
 
 * 既然每个worker都拷贝了一份dataset，为什么num worker被kill掉的时候没有触发其中dataset的析构方法？
 * workers per gpu 写1的时候是两个，写2的时候是3个？？？
+* mp.lock得是一个对象才有用，创建的两个对象是没用的！
+
+## references
+
+[1] [问题的普遍性](https://zhuanlan.zhihu.com/p/357809861)
+[2] [lmdb文档]
+[3] [multiprocessing文档]
--- lmdb_containers/lmdb_list.py
+++ lmdb_containers/lmdb_list.py
@@ -1,76 +1,68 @@
 import lmdb
 from collections.abc import Sequence
+from collections import Iterable
 import multiprocessing as mp
 from multiprocessing import Process, Manager, Value
 import os
 import shutil
-
-lock = mp.RLock()
+from time import sleep
 
 
 class LMDBList(Sequence):
+    r"""
+    List `cached' by LMDB. LMDBList consumes little memory but is expected
+     to be used like a regular python list.
+
+    Sometimes, we have a very large list in our memory, which may cause
+    OOM problem. In LMDBList, we solve the memory problem at the cost
+    of performance.
+
+    Arguments:
+        raw_list (Iterable): list (anything iterable) to be cached,
+        lmdb_path (str): path to save the lmdb,
+        map_write (callable, optional): Since both the key&value in lmdb
+            should be strings, we need to map the element in `raw_list` to a
+            string.
+        map_read (callable, optional): Map function when we call lmdblist[idx]
+            to read an element from the LMDBList. Typically, the following
+            equation holds:
+                x == map_read(map_write(x))
+
+    .. warning:: I do not implement `append` and `extend`. I can do this but
+        I think it is unnecessary currently. LMDBList is designed as a read-
+        only list at the most of the time.
+    .. warning::
+    """
+
     def __init__(self, raw_list, lmdb_path, map_write=lambda x: x,
                  map_read=lambda x: x):
-        n = len(raw_list)
-        self.length = n
+        self.length = None
         self.lmdb_path = lmdb_path
 
         self.map_write = map_write
         self.map_read = map_read
 
         self.env = None
-        lock.acquire()  # for sync dataset in different nodes
-        self.create_lmdb(raw_list)
-        lock.release()
 
     def append(self, item):
-        raise NotImplementedError(f"{self.__class__} do not support online write")
+        raise NotImplementedError(f"{self.__class__} do not support online writing")
         # with self.db.begin(write=True) as txn:
         #     txn.put(str(self.size).encode(), self.map_write(item))
         #     self.size += 1
 
-    def add_process_count(self):
-        lock.acquire()
-        env = lmdb.open(self.lmdb_path, map_size=1099511627776)
-        with env.begin(write=True) as txn:
-            n_dataset = int(txn.get('n_dataset'.encode()).decode()) + 1
-            txn.put('n_dataset'.encode(), str(n_dataset).encode())
-            print(f'add 1 reading process, now process: {n_dataset}')
-        env.close()
-        lock.release()
-
-    def minus_process_count(self):
-        lock.acquire()
-        env = lmdb.open(self.lmdb_path, map_size=1099511627776)
-        with env.begin(write=True) as txn:
-            n_dataset = int(txn.get('n_dataset'.encode()).decode())
-            if n_dataset == 0:
-                raise ValueError('n_dataset is 0 before minor!!!')
-            n_dataset -= 1
-            txn.put('n_dataset'.encode(), str(n_dataset).encode())
-            print(f'minus 1 process, now process: {n_dataset}')
-        env.close()
-        # if n_dataset == 0:
-        #     print('no process, rm lmdb')
-        #     shutil.rmtree(self.lmdb_path)
-        lock.release()
-
     def create_lmdb(self, raw_list):
-        if os.path.exists(self.lmdb_path):
-            print('lmdb exist, skip creating')
-            return
-
-        print('Writing lmdb')
         env = lmdb.open(self.lmdb_path, map_size=1099511627776)
         with env.begin(write=True) as txn:
+            n = 0
             for i, content in enumerate(raw_list):
                 content = self.map_write(content)
                 txn.put(str(i).encode(), content.encode())
-            txn.put('n_dataset'.encode(), str(1).encode())
+                n += 1
+        self.length = n
         env.close()
 
     def open_lmdb(self):
-        self.add_process_count()
+        # self.add_process_count()
         self.env = lmdb.open(self.lmdb_path, map_size=1099511627776, lock=False, readonly=True, readahead=False,
                              meminit=False, max_readers=512)
 
@@ -94,7 +86,72 @@ class LMDBList(Sequence):
         data = self.map_read(data)
         return data
 
-    def __del__(self):
-        self.minus_process_count()
-        #     print('dataset deleted, rm lmdb')
-        #     shutil.rmtree(self.lmdb_path)
+    # def __del__(self):
+    #     self.minus_process_count()
+    #     print('dataset deleted, rm lmdb')
+    #     shutil.rmtree(self.lmdb_path)
+
+
+class LMDBListMP(Sequence):
+    def __init__(self, raw_list, lmdb_path, map_write=lambda x: x,
+                 map_read=lambda x: x, is_main=True):
+        # raw list: anything iterable
+        n = len(raw_list)  # cache a list
+
+        self.length = n
+        self.lmdb_path = lmdb_path
+
+        self.map_write = map_write
+        self.map_read = map_read
+
+        self.env = None
+        if is_main:
+            print('main process writing lmdb')
+            self.create_lmdb(raw_list)
+        else:
+            print('not main process skip writing')
+
+    def append(self, item):
+        raise NotImplementedError(f"{self.__class__} do not support online writing")
+        # with self.db.begin(write=True) as txn:
+        #     txn.put(str(self.size).encode(), self.map_write(item))
+        #     self.size += 1
+
+    def create_lmdb(self, raw_list):
+        env = lmdb.open(self.lmdb_path, map_size=1099511627776)
+        with env.begin(write=True) as txn:
+            for i, content in enumerate(raw_list):
+                content = self.map_write(content)
+                txn.put(str(i).encode(), content.encode())
+            txn.put('n_dataset'.encode(), str(1).encode())
+        env.close()
+
+    def open_lmdb(self):
+        # self.add_process_count()
+        self.env = lmdb.open(self.lmdb_path, map_size=1099511627776, lock=False, readonly=True, readahead=False,
+                             meminit=False, max_readers=512)
+
+    def close_lmdb(self):
+        self.env.close()
+        self.env = None
+
+    def __len__(self):
+        return self.length
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+    def __getitem__(self, index):
+        if self.env is None:
+            self.open_lmdb()
+        with self.env.begin() as txn:
+            data = txn.get(str(index).encode()).decode()
+        # self.close_lmdb()
+        data = self.map_read(data)
+        return data
+
+    # def __del__(self):
+    #     self.minus_process_count()
+    #     print('dataset deleted, rm lmdb')
+    #     shutil.rmtree(self.lmdb_path)
